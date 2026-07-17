a0e061a2d23173020840fe6e2cfb1d0f2c82dc31
Víctor Altamirano <v.a.i@hotmail.com>
2026-07-17 01:56:28 -0600

    commit

--- dapp/metro.config.js
+++ dapp/metro.config.js
@@ -3,6 +3,17 @@ const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
+const { transformer, resolver } = config;
+config.transformer = {
+  ...transformer,
+  babelTransformerPath: require.resolve("react-native-svg-transformer/expo")
+};
+config.resolver = {
+  ...resolver,
+  assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
+  sourceExts: [...resolver.sourceExts, "svg"]
+};
+
 config.resolver.resolveRequest = (context, moduleName, platform) => {
   if (moduleName === 'uuid' || moduleName.startsWith('uuid/')) {
     return {
